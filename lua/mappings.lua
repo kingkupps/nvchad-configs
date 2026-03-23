58844868f9f9b857b5883d8ea4e122e4353bffcc
Jamie Kuppel <jamiekuppel@gmail.com>
2026-03-22 21:52:07 -0400

    Telescope lsp references keybind

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,3 +6,5 @@ local map = vim.keymap.set
 
 map("v", "J", ":m '>+1<CR>gv=gv")
 map("v", "K", ":m '<-2<CR>gv=gv")
+
+map("n", "<leader>fr", "<cmd>Telescope lsp_references<CR>", { desc = "telescope lsp references" })
