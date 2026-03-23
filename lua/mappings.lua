f123081bfecb0ea34ca868e3582b0796aa64d0db
Jamie Kuppel <jamiekuppel@gmail.com>
2026-03-22 22:23:13 -0400

    Ctrl-s for exiting insert mode and saving in one go

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,6 @@ local map = vim.keymap.set
 map("v", "J", ":m '>+1<CR>gv=gv")
 map("v", "K", ":m '<-2<CR>gv=gv")
 
+map("i", "<C-s>", "<Esc>:w<CR>", { desc = "save file" })
+
 map("n", "<leader>fr", "<cmd>Telescope lsp_references<CR>", { desc = "telescope lsp references" })
