0415ed5caa38adc4e0f41f0cf9a17faffcc05b76
Jamie Kuppel <jamie@vannevarlabs.com>
2026-04-17 08:44:26 -0400

    Exit all buffers key map, add more wrap text mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,11 +4,21 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+-- Drag highlighted code
 map("v", "J", ":m '>+1<CR>gv=gv")
 map("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Save and exit insert mode
 map("i", "<C-s>", "<Esc>:w<CR>", { desc = "save file" })
 
+-- Search for references the right way
 map("n", "<leader>fr", "<cmd>Telescope lsp_references<CR>", { desc = "telescope lsp references" })
 
+-- Auto wrap with symbol
 map("v", "{", "<Esc>`>a}<Esc>`<i{<Esc>", { desc = "surround selection with {}" })
+map("v", "(", "<Esc>`>a)<Esc>`<i(<Esc>", { desc = "surround selection with ()" })
+map("v", "'", "<Esc>`>a'<Esc>`<i'<Esc>", { desc = "surround selection with ''" })
+map("v", '"', "<Esc>`>a\"<Esc>`<i\"<Esc>", { desc = "surround selection with \"\"" })
+
+-- Exit all buffers
+map("n", "<leader>qq", ":qa<CR>")
